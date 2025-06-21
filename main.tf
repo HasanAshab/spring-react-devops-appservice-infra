2d089629f2fbdb08a331b3bd9edeced1dd3edf71
Your Name <you@example.com>
2025-06-21 17:42:02 +0600

    backup

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 resource "azurerm_resource_group" "main" {
   name     = "rg-${local.project_name}-${terraform.workspace}"
   location = var.location
-  tags     = local.tags
+  tags = {
+    Environment = terraform.workspace
+    Service     = local.project_name
+  }
 }
 
 locals {
@@ -14,7 +17,10 @@ resource "azurerm_kubernetes_cluster" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   dns_prefix          = local.dns_prefix
-  tags                = local.tags
+  tags = {
+    Environment = terraform.workspace
+    Service     = "aks"
+  }
 
   default_node_pool {
     name       = "default"
