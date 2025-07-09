6fb1225dd5c9dcef0e2fc130f0f652cdaedf7f8e
Your Name <you@example.com>
2025-07-09 15:38:40 +0600

    backup

--- modules/database/locals.tf
+++ modules/database/locals.tf
@@ -1,6 +1,5 @@
 locals {
   naming_suffix                = ["db"]
-  pe_naming_suffix             = ["pe"]
   dns_zone_name                = "privatelink.mysql.database.azure.com"
   geo_redundant_backup_enabled = true
   public_network_access        = "Disabled"
