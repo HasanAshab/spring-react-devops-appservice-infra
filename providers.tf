159da37ea8a6e61c639d4216c76bfedddbe7019d
Your Name <you@example.com>
2025-07-14 17:46:43 +0600

    backup

--- providers.tf
+++ providers.tf
@@ -4,24 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "4.33.0"
     }
-    http = {
-      source  = "hashicorp/http"
-      version = "3.5.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.7.2"
-    }
   }
   required_version = " >= 1.10.0"
 }
 
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
+  features {}
 }
-
-provider "random" {}
